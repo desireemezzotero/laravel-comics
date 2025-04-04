7503bd0c69d05b4ebe1fd940651d88eea5dfe9a4
desireemezzotero <desireemezzotero1@gmail.com>
2025-04-04 18:19:34 +0200

    add footer col

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,183 @@
-<header>
-  <h5>ciao sono footer</h5>
-</header>
+<footer>
+ <div class="container">
+  <div class="row w-50 pt-5">
+
+    <div class="col-4">
+
+      <div>
+        <ul>
+          <h5 class="text-white font-weight-bold">DC COMICS</h5>
+          <li>
+            <a href="#" class="text-secondary">
+              Characters
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Comics
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Movies
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              TV
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Games
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Videos
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              News
+            </a>
+          </li>
+        </ul>
+      </div>{{-- dc comics --}}
+  
+      <div>
+        <ul>
+          <h5 class="text-white font-weight-bold">SHOP</h5>
+          <li>
+            <a href="#" class="text-secondary">
+             Shop DC
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Shop DC Collectibles
+            </a>
+          </li>
+        </ul>
+      </div>{{-- shop --}}
+    </div>{{-- col prima  --}}
+
+    <div class="col-4">
+      <div>
+        <ul>
+          <h5 class="text-white font-weight-bold"> DC</h5>
+          <li>
+            <a href="#" class="text-secondary">
+            Terms Of Use
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              Privacy policy(New)
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Ad Choices
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Advertising
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Jobs
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Subscriptions
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Talent Workshops
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              CPSC Certificates 
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Ratings
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Shop Help
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              Contact Us
+            </a>
+          </li>
+
+        </ul>
+      </div>{{-- dc --}}
+    </div> {{-- col seconda --}}
+
+    <div class="col-4">
+      <div>
+        <ul>
+          <h5 class="text-white font-weight-bold">SITES</h5>
+          <li>
+            <a href="#" class="text-secondary">
+            DC
+            </a>
+          </li>
+  
+          <li>
+            <a href="#"class="text-secondary">
+              MAD Magazine
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              DC Kids
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+             Dc Universe
+            </a>
+          </li>
+
+          <li>
+            <a href="#"class="text-secondary">
+              DC Power Visa
+            </a>
+          </li>
+      </div> {{-- sites --}}
+    </div> {{-- col terza --}}
+  </div> {{-- div row --}}
+ </div>
+</footer>
